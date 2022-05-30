6dd965a8af3b70070f44811b9f8090d0cda4be43
xuzheng465 <xuzheng465@gmail.com>
2022-05-30 11:48:06 +0800

    recipe data model

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func main() {
 	router := gin.Default()
 	router.Run()
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tag          []string  `json:"tag"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"published_at"`
 }
